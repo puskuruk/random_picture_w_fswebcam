a94d0224437179ef0d0dc20d1795b8b2b61e1241
Puskuruk <tamayeseruysal@gmail.com>
2019-06-19 01:53:54 +0300

    fixed typo

--- random_pic.sh
+++ random_pic.sh
@@ -1,5 +1,5 @@
 while true
 	do
 		DATE=$(date +"%Y-%m-%d_%H%M")
-		sleep $((1 + RANDOM % 100))m && fswebcam -r 1280x720 --no-banner $HOME/pictures/$DATE.jpeg
+		sleep $((1 + RANDOM % 100))m && fswebcam -r 1280x720 --no-banner $HOME/Pictures/$DATE.jpeg
 	done
